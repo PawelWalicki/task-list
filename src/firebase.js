794a376b3cadc5b6c16245199f7c28dcf0314448
Pawel Walicki <pawel.walicki89@interia.pl>
2025-03-16 22:38:01 +0100

    add env

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB_UjDPPwLfKgMyrQmdwm26yaaP5OW2mJE",
-  authDomain: "kanban-d5e00.firebaseapp.com",
-  databaseURL: "https://kanban-d5e00-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "kanban-d5e00",
-  storageBucket: "kanban-d5e00.firebasestorage.app",
-  messagingSenderId: "681895031106",
-  appId: "1:681895031106:web:303d2f8e040a5b093d58db"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
