10450c766bfaebc9c867ebdc61d378007242cfe0
Pawel Walicki <pawel.walicki89@interia.pl>
2025-03-21 18:58:53 +0100

    add password veryfication

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -3,33 +3,60 @@ import { auth } from '../firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import './Registration.css'
 import { Button, Container, Grid2, TextField } from '@mui/material';
+import { Footer } from '../components/Footer';
 
 
 export const Registration = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
     const [notice, setNotice] = useState("");
+
+
+    const validatePassword = () => {
+        let isValid = true;
+        if (password !== confirmPassword) {
+            isValid = false
+            setPasswordError("Passowords are not the same")
+        } else {
+            setPasswordError('')
+        }
+        return isValid
+    }
 
     const onSubmit = async (e) => {
         e.preventDefault()
-        await createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredentials) => {
-                const user = userCredentials.user
-                console.log(user)
-            })
-            .catch(e => {
-                setNotice("Requirements for email or password are not met!")
-            })
+        setNotice("")
+        setPasswordError("")
+        if (validatePassword()) {
+
+            await createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredentials) => {
+                    const user = userCredentials.user
+                    console.log(user)
+                })
+                .catch(e => {
+                    setNotice("Requirements for email or password are not met!")
+                })
+        } else {
+             setNotice("Passords does not match")
+        }
+
     }
+
+
 
     return (
         <Container>
             <h1 className='title'> Registration </h1>
-            {"" !== notice &&
-                <div role="alert">
-                    {notice}
-                </div>
-            }
+            <div className='alert'>
+                {"" !== notice &&
+                    <div role="alert">
+                        {notice}
+                    </div>
+                }
+            </div>
             <Grid2 container spacing={2} size="grow" display="flex" justifyContent="center" alignItems="center">
                 <Grid2 size={7}>
                     <TextField
@@ -75,6 +102,30 @@ export const Registration = () => {
                         }}
                     />
                 </Grid2>
+                <Grid2 size={7}>
+                    <TextField
+                        type="password"
+                        label="Confirm password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                        fullWidth
+                        error={passwordError !== ""}
+                        helperText={passwordError}
+                        sx={{
+                            "& .MuiOutlinedInput-root": {
+                                color: "#417f9e",
+                                "& .MuiOutlinedInput-notchedOutline": {
+                                    borderColor: "#417f9e",
+                                    borderWidth: "2px",
+                                },
+                            },
+                            "& .MuiInputLabel-outlined": {
+                                color: "#417f9e",
+                            },
+                        }}
+                    />
+                </Grid2>
                 <Grid2 display="flex" justifyContent="center" alignItems="center" size={7}>
                     <Button
                         className="button"
@@ -88,6 +139,7 @@ export const Registration = () => {
                     </Button>
                 </Grid2>
             </Grid2>
+            <Footer />
         </Container>
     )
 }
